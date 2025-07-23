73a9d81153adde6666ad66c8e5f972f8e9bee924
abhishek <abhishekkadavergu944@gmail.com>
2025-07-23 09:16:45 +0530

    added edit and delete function to the blog

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -1,29 +1,28 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+
 export async function DELETE(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params;
+  const { id } = await context.params;
 
   try {
     await prisma.blog.delete({ where: { id } });
     return NextResponse.json({ message: "Deleted successfully" });
-  } catch (error) {
+  } catch {
     return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
   }
 }
 
+
 export async function PUT(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params;
+  const { id } = await context.params;
   const data = await req.json();
-
-  console.log("Updating blog with ID:", id);
-  console.log("Received data:", data);
 
   try {
     const updated = await prisma.blog.update({
@@ -39,17 +38,17 @@ export async function PUT(
     });
 
     return NextResponse.json(updated);
-  } catch (err) {
-    console.error("Error updating blog:", err);
+  } catch {
     return NextResponse.json({ error: "Failed to update blog" }, { status: 500 });
   }
 }
 
+
 export async function GET(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params;
+  const { id } = await context.params;
 
   try {
     const blog = await prisma.blog.findUnique({
@@ -61,7 +60,8 @@ export async function GET(
     }
 
     return NextResponse.json(blog);
-  } catch (error) {
+  } catch  {
     return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
   }
 }
+
